6f90bb07235e7d780fa5edf0e693b1eeab5e4a19
cindy-tran <cindyntran96@gmail.com>
2018-09-30 15:32:12 -0700

    fixed bug

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+// Initialize the time
 var seconds = 0;
 var minutes = 0;
 
@@ -49,39 +50,31 @@ var alarmOn = false;
 var sessionMax = 0; 
 
 var timer = new Pomodoro(function() {
-    console.log(minutes);
-    console.log(seconds);
-    console.log(sessionMax);
-    if(alarmOn == false) {
-        var currentState = document.getElementById("state").textContent;
-        if (currentState == "Break") {
-            sessionMax = parseInt(document.getElementById("break-length").textContent);
-        }
-        else {
-            sessionMax = parseInt(document.getElementById("session-length").textContent);
-        }
+    var currentState = document.getElementById("state").textContent;
+    if (currentState == "Break") {
+        sessionMax = parseInt(document.getElementById("break-length").textContent);
+    }
+    else {
+        sessionMax = parseInt(document.getElementById("session-length").textContent);
+    }
+    if(seconds == 60) {
+        minutes++;
+        seconds=0;
         if (parseInt(minutes) == sessionMax && parseInt(seconds) == 0) {
             alert("Session is over!");
-            alarmOn = true;
+            timer.reset(1000);
+            timer.pause();
         }
-        if(seconds == 59) {
-            minutes++;
-            seconds=0;
-        }  
-
-
-        if (minutes < 10) {
-            minTime = "0" + minutes;
-        }
-        if(seconds < 10) {
-            timeDisplay(minTime + ":0" + seconds++);
-        }
-        else {
-            timeDisplay(minTime + ":" + seconds++);
-        }
-
+    }  
+    if (minutes < 10) {
+        minTime = "0" + minutes;
     }
-
+    if(seconds < 10) {
+        timeDisplay(minTime + ":0" + seconds++);
+    }
+    else {
+        timeDisplay(minTime + ":" + seconds++);
+    }
 }, 1000);
 
 timer.pause();
@@ -103,8 +96,6 @@ document.getElementById("play").addEventListener("click", function(e) {
 document.getElementById("pause").addEventListener("click", function(e) {
     timer.pause();
 });
-
-
 document.getElementById("mug").addEventListener("click", function(e) {
     timer.break();
     var newTime = 1000;
@@ -112,9 +103,6 @@ document.getElementById("mug").addEventListener("click", function(e) {
         timer.reset(newTime);
     }
 });
-
-
-
 document.getElementById("increaseBreak").addEventListener("click", function(e) {
     document.getElementById("break-length").textContent = parseInt(document.getElementById("break-length").textContent) + 1
 });
@@ -125,7 +113,6 @@ document.getElementById("decreaseBreak").addEventListener("click", function(e) {
         document.getElementById("break-length").textContent--;
     }
 });
-
 document.getElementById("increaseSession").addEventListener("click", function(e) {
     document.getElementById("session-length").textContent = parseInt(document.getElementById("session-length").textContent) + 1;
 });
